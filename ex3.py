6d4088763bff66b509bb04d3523ceb110d4a1daa
xiangcl <xiangcl@yahoo.com>
2017-02-06 10:18:39 +0800

    为ex3.py添加注释

--- ex3.py
+++ ex3.py
@@ -1,23 +1,34 @@
+# -*- coding:utf-8 -*-
+# 打印文本
 print "I will now count my chickens:"
 
+# 打印文本，并计算结果
 print "Hens", 25 + 30 / 6
 print "Roosters", 100 - 25 * 3 % 4
 
+# 打印文本
 print "Now I will count the eggs:"
 
+# 计算结果
 print 3 + 2 + 1 - 5 + 4 % 2 - 1 / 4 + 6
 
+# 打印文本
 print "Is it true that 3 + 2 < 5 - 7?"
 
+# 计算
 print 3 + 2 < 5 - 7
 
+# 计算并打印
 print "What is 3 + 2?", 3 + 2
 print "What is 5 - 7?", 5 - 7
 
+# 打印文本
 print "Oh, that's why it's False."
 
+# 打印文本
 print "How about some more."
 
+# 判断boolean值，并打印
 print "Is it greater?", 5 > -2
 print "Is it greater or equal?", 5 >= -2
 print "Is it less or equal?", 5 <= -2

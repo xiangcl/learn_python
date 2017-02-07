aef96800ba9eff95ee73474e9888681a84745f48
xiangcl <xiangcl@yahoo.com>
2017-02-07 14:50:39 +0800

    创建 打印，打印，打印 ex9.py,并添加注释

---
+++ ex9.py
@@ -0,0 +1,18 @@
+# -*- coding:utf-8 -*-
+# Here's some new strange stuff,remember type it exactly.
+
+# 定义变量，并赋值
+days = "Mon Tue Wed Thu Fri Sat Sun"
+months = "Jan\nFeb\nMar\nApr\nMay\nJun\nJul\nAug"
+
+# 打印字符串及变量
+print "Here are the days: ", days
+print "Here are the months: ", months
+
+# 打印字符串
+print """
+There's something going on here.
+With the three double-quotes.
+We'll be able to type as much as we like.
+Even "4" lines if we want, or 5, or 6.
+"""

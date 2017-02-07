4ea8ec5092ff26111ace5c775ece7513f5b5d67b
xiangcl <xiangcl@yahoo.com>
2017-02-07 15:29:53 +0800

    添加注释

--- ex10.py
+++ ex10.py
@@ -1,14 +1,15 @@
 # -*- coding:utf-8 -*-
 # 第一种方法
-print "I am 6'2\" tall."
-print 'I am 6\'2" tall.'
+print "I am 6'2\" tall."	# 将字符中的双引号转义
+print 'I am 6\'2" tall.'	# 将字符中的单引号转义
 
 
-# 第一种方法
-tabby_cat = "\t I'm tabbed in."
-persian_cat = "I'm split\non a line."
-backslash_cat = "I'm \\ a \\ cat."
+# 第二种方法
+tabby_cat = "\t I'm tabbed in."		# 将tab转义
+persian_cat = "I'm split\non a line."	# 转义换行
+backslash_cat = "I'm \\ a \\ cat."	# 将 \ 转义
 
+# 使用三引号输出，用了tab以及换行的转义
 fat_cat = """
 I'll do a list:
 \t* Cat food
@@ -16,6 +17,7 @@ I'll do a list:
 \t* Catnip\n\t* Grass
 """
 
+# 打印变量
 print tabby_cat
 print persian_cat
 print backslash_cat

e47962a73ada8f68ac0037841df36923a511d96a
xiangcl <xiangcl@yahoo.com>
2017-02-07 13:44:23 +0800

    为ex8.py添加注释

--- ex8.py
+++ ex8.py
@@ -1,13 +1,18 @@
 # -*- coding:utf-8 -*-
+# 定义变量，并使用格式化输出
 formatter = "%r %r %r %r"
 
+# 打印数字格式化输出
 print formatter % (1, 2, 3, 4)
+# 打印文字格式化输出
 print formatter % ("one", "two", "three", "four")
 print formatter % (True, False, False, True)
+# 调用自身输出
 print formatter % (formatter, formatter, formatter, formatter)
+
 print formatter % (
 	"I had this thing.",
 	"That you could type up right.",
-	"But it didn't sing.",
+	"But it didn't sing.", # 当碰到双引号内部有单引号时，%r 会格式化输出单引号为双引号
 	"So I said goodnight."
 )

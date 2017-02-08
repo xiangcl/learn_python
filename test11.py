09475ce3ee574f55889d389ec1d250eddeab344a
xiangcl <xiangcl@yahoo.com>
2017-02-08 10:36:36 +0800

    创建test11.py的测试代码

---
+++ test11.py
@@ -0,0 +1,11 @@
+# -*- coding:utf-8 -*-
+
+# 这是一段关于 raw_input() 应用的代码
+print "请输入你的姓名:",
+name = raw_input()
+print "请输入你的年龄:",
+age = raw_input()
+print "请输入你的身高:",
+height = raw_input()
+
+print "So, you're name: %s, %s old, %s heavy." % (name, age, height)

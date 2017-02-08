b1fc67c0b7770f5dce598d4fdbe7fc3d5eb6831c
xiangcl <xiangcl@yahoo.com>
2017-02-08 10:17:04 +0800

    为ex11.py添加注释

--- ex11.py
+++ ex11.py
@@ -1,10 +1,10 @@
 # -*- coding:utf-8 -*-
 
-print "How old are you?",
-age = raw_input()
-print "How tall are you?",
-height = raw_input()
-print "How much do you weight?",
-weight = raw_input()
+print "How old are you?",	# 打印字符串
+age = raw_input()	# 定义变量，并将输入的值赋给变量
+print "How tall are you?",	# 打印字符串
+height = raw_input()	# 定义变量，并将输入的值赋给变量
+print "How much do you weight?",	# 打印字符串
+weight = raw_input()	# 定义变量，并将输入的值赋给变量
 
-print "So, you're %r old, %r tall and %r heavy." % (age, height, weight)
+print "So, you're %r old, %r tall and %r heavy." % (age, height, weight)	# 打印字符串，并使用 %r 输出格式化变量的字符

06fad8620e10ab81787ddc0d9b2bab411e41e970
xiangcl <xiangcl@yahoo.com>
2017-02-08 13:39:16 +0800

    修改，添加年龄信息

--- ex14.py
+++ ex14.py
@@ -2,10 +2,12 @@
 
 from sys import argv	# 将sys模块import
 
-script, user_name = argv	# 将argv中的东西解包，将所有的参数依次赋予左边的变量名
+script, user_name, age = argv	# 将argv中的东西解包，将所有的参数依次赋予左边的变量名
 prompt = '#  '	# 定义提示符变量
 
-print "Hi %s, I'm the %s script." % (user_name, script)		# 打印字符串
+print "Hi %s, I'm the %s script." % (user_name,  script)		# 打印字符串
+print "你今天年 %d 岁了！" % (int(age))	#添加年龄信息
+
 print "I'd like to ask you a few questions."	# 打印字符串
 print "Do you like me %s?" % user_name	# 打印字符串
 likes = raw_input(prompt)	# 根据上面的提问，给出提示符等待输入，将输入的结果赋值给变量likes
@@ -21,4 +23,5 @@ print """
 Alright, so you said %r about liking me.
 You live in %r. Not sure where that is.
 And you have a %r computer. Nice.
-""" % (likes, lives, computer)
+Age is %d
+""" % (likes, lives, computer, int(age))

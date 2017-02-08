8008c2a749fa23cfc39cb9e14f23051c423dd49b
xiangcl <xiangcl@yahoo.com>
2017-02-08 11:21:54 +0800

    为ex13.py添加注释

--- ex13.py
+++ ex13.py
@@ -1,9 +1,10 @@
 # -*- coding:utf-8 -*-
 
-from sys import argv
+from sys import argv	# 导入 sys 模块
 
-script, first, second, third = argv
+script, first, second, third = argv		# 将argv 进行unpack,并将参数赋予左边的变量名
 
+# 打印字符串以及变量
 print "The script is called:", script
 print "Your first variable is:", first
 print "Your second variable is:", second

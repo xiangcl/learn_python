54d9e971a1546e0367139eb15f561a9fc4768366
xiangcl <xiangcl@yahoo.com>
2017-02-08 11:36:37 +0800

    <练习13.参数，解包，变量>的test

---
+++ test13.py
@@ -0,0 +1,13 @@
+# -*- coding:utf-8 -*-
+from sys import argv
+
+a, b, c = argv
+
+print "这个脚本名是: ", a
+print "你喜欢犀利哥么? ", b
+# print "你喜欢我么? ", c
+
+c = raw_input ("你真的喜欢凤姐么? ")
+
+print "是的，我真的%s" %(c)
+

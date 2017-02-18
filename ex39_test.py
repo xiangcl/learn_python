8ce4df8f4c829ced88de6e9e9f078e68abae1af6
xiangcl <xiangcl@yahoo.com>
2017-02-18 13:34:12 +0800

    完成练习39的内容

--- ex39_test.py
+++ ex39_test.py
@@ -3,7 +3,7 @@
 import hashmap
 
 # create a mapping of state to abbreviation
-state = hashmap.new()
+states = hashmap.new()
 hashmap.set(states, 'Oregon', 'OR')
 hashmap.set(states, 'Florida', 'FL')
 hashmap.set(states, 'California', 'CA')
@@ -24,3 +24,32 @@ hashmap.set(cities, 'OR', 'Portland')
 print '-' * 10
 print "NY State has: %s" % hashmap.get(cities, 'NY')
 print "OR State has: %s" % hashmap.get(cities, 'OR')
+
+# print some states
+print '-' * 10
+print "Michigan's abbreviation is: %s" % hashmap.get(states, 'Michigan')
+print "Florida's abbreviation is: %s" % hashmap.get(states, 'Florida')
+
+# do it by using the state then cities dict
+print '-' * 10
+print "Michigan has: %s" % hashmap.get(cities, hashmap.get(states, 'Michigan'))
+print "Florida has: %s" % hashmap.get(cities, hashmap.get(states, 'Florida'))
+
+# print every state abbreviation
+print '-' * 10
+hashmap.list(states)
+
+# print every city in state
+print '-' * 10
+hashmap.list(cities)
+
+print '-' * 10
+state = hashmap.get(states, 'Texas')
+
+if not state:
+	print "Sorry, no Texas."
+
+# default values using ||= with the nil result
+# can you do this on ome line?
+city = hashmap.get(cities, 'TX', 'Dose Not Exist')
+print "The city for the state 'TX' is: %s" % city

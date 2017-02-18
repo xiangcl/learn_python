f68c538dc38a6cd9c2b24c517b6b588850e2ca0f
xiangcl <xiangcl@yahoo.com>
2017-02-18 15:06:26 +0800

    添加注释

--- ex40.py
+++ ex40.py
@@ -1,24 +1,26 @@
 # -*- coding:utf-8 -*-
 
+# 创建 cities 的字典
 cities = {'CA': 'San Francisco', 'MI': 'Detroit', 'FL': 'Jacksonville'}
 
+# 在 cities 中添加元素
 cities['NY'] = 'New York'
 cities['OR'] = 'Portland'
 
-def find_city(themap, state):
-	if state in themap:
+def find_city(themap, state):	# 创建一个find_city()的函数
+	if state in themap:			# 对元素进行判断
 		return themap[state]
 	else:
 		return "Not found."
 
 # ok pay attention!
-cities['_find'] = find_city
+cities['_find'] = find_city		# 
 
-while True:
-	print "State? (ENTER to quit)",
+while True:						# 使用while循环
+	print "State? (ENTER to quit)",		# 打印字符提示
 	state = raw_input("> ")
-	if not state: break
+	if not state: break				# 如果state没有接受则跳出循环
 
 	# this line is the most important ever! study!
-	city_found = cities['_find'](cities, state)
-	print city_found
+	city_found = cities['_find'](cities, state)		# 调用find_city函数
+	print city_found	# 打印结果
